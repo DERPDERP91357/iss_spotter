c860a54136ab4c3eb07993c4a2f9c17de839a941
Daniel Suen <danielsuen200791357@gmail.com>
2022-08-05 02:02:49 +0000

    created iss tracker function

--- index.js
+++ index.js
@@ -7,28 +7,42 @@
 // Next pass at Fri Jun 01 2021 16:12:35 GMT-0700 (Pacific Daylight Time) for 648 seconds!
 // Next pass at Fri Jun 01 2021 17:49:29 GMT-0700 (Pacific Daylight Time) for 648 seconds!
 // Next pass at Fri Jun 01 2021 19:26:12 GMT-0700 (Pacific Daylight Time) for 643 seconds!
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const {
+  // fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, 
+  nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error1, ip) => {
-  if (error1) {
-    console.log("It didn't work!" , error1);
-    return;
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// fetchCoordsByIP(ip, (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned Data:' , coordinates);
+// });
+
+// fetchISSFlyOverTimes(coordinates, (error, times)=> {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned Times:' , times);
+// });
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
   }
-
-  console.log('It worked! Returned IP:' , ip);
-  fetchCoordsByIP(ip, (error, coordinates) => {
-    if (error) {
-      console.log("It didn't work!" , error);
-      return;
-    }
-    console.log('It worked! Returned Data:' , coordinates);
-    fetchISSFlyOverTimes(coordinates, (error, times)=> {
-      if (error) {
-        console.log("It didn't work!" , error);
-        return;
-      }
-      console.log('It worked! Returned Times:' , times);
-    });
-  });
+  for (let time of passTimes) {
+    var date = new Date(0);
+    date.setUTCSeconds(time.risetime);
+    console.log(`Next pass at ${date} for ${time.duration} seconds!`)
+  }
 });
 
